f3017e3c1b8229b15e0a512f7755ef63a484d018
pondooo5800 <pondooo5800@gmail.com>
2020-08-18 21:21:00 +0700

    Update Dashboard_user.php

--- application/controllers/Dashboard_user.php
+++ application/controllers/Dashboard_user.php
@@ -154,11 +154,10 @@ class Dashboard_user extends CRUD_Controller
 		$this->data['chart_labels_calo'] = $chart_labels_calo;
 		$this->data['chart_bmi'] = json_encode($chart_bmi);
 		$this->data['chart_bmr'] = json_encode($chart_bmr);
-		$this->data['chart_bmr_credit'] = @$chart_bmr[count($chart_bmr) - 1]['y'] . ' kcal/day';
+		$labels_bmr_credit = @$chart_bmr[count($chart_bmr) - 1]['y'];
+		$this->data['chart_bmr_credit'] = number_format($labels_bmr_credit, 2) . ' kcal/day';
 		$this->data['chart_calo'] = json_encode($chart_calo);
 		$this->data['chart_calo_credit'] = @$chart_calo[count($chart_calo) - 1]['y'] . ' kcal/day';
-		// die(print_r($chartrows_calo));
-
 		$this->dashboard();
 	}
 
